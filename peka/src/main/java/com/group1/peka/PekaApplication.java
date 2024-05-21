fb18c3df292fbcd5460b2039379a354700a4a5be
liwangivone <liwangivone@gmail.com>
2024-05-21 09:23:13 +0800

    setup project spring

---
+++ peka/src/main/java/com/group1/peka/PekaApplication.java
@@ -0,0 +1,13 @@
+package com.group1.peka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekaApplication.class, args);
+	}
+
+}
